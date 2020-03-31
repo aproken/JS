1dee4d5f7b943f2638828644087b40afd244702f
aproken <aproken@yandex.ru>
2020-03-31 20:38:26 +0300

    feat: add exercise 1b

--- lesson03-hard/js/script.js
+++ lesson03-hard/js/script.js
@@ -2,11 +2,14 @@ let lang = prompt('Выберете язык: ru или en', 'ru');
     daysRu = ['Воскресенье','Понедельник','Вторник','Среда','Четверг','Пятница','Суббота'],
     daysEn = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
-if (lang === 'ru') {
-  alert(daysRu);
-} else if(lang === 'en') {
-  alert(daysEn);
-} else {
-  alert('Выберете ru или en');
+switch(lang) {
+  case 'ru':
+    alert(daysRu);
+    break;
+  case 'en': 
+    alert(daysEn);
+    break;
+  default: 
+    alert('Выберете ru или en');
 }
 
