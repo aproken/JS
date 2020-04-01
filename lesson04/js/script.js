5b00ae496bc97caa0e3b7216a038165c82a28423
Natalia <aproken@yandex.ru>
2020-04-01 22:25:53 +0300

    fix: add function showshowTypeOf

--- lesson04/js/script.js
+++ lesson04/js/script.js
@@ -1,32 +1,5 @@
 'use strict';
 
-function getStatusIncome() {
-  if (budgetDay >= 1200) {
-    return 'У вас высокий уровень дохода';
-  } else if ((budgetDay < 1200) && (budgetDay > 600)) {
-    return 'У вас средний уровень дохода';
-  } else if ((budgetDay <= 600) && (budgetDay >=0 )) {
-    return 'К сожалению у вас уровень дохода ниже среднего';
-  } else {
-    return 'Что то пошло не так';
-  }
-};
-
-//Функция возвращает сумму всех обязательных расходов за месяц
-function getExpensesMonth(expenses) {
-  return expenses.reduce((accume, current) => accume + current);
-}
-
-//Функция возвращает Накопления за месяц (Доходы минус расходы)
-function getAccumulatedMonth(income, expense) {
-  return income - expense;
-}
-
-//Подсчитывает за какой период будет достигнута цель
-function getTargetMonth(amount, expenses) {
-  return amount / expenses;
-}
-
 let money = prompt('Ваш месячный доход?', 60000),
     income = 'Играю на фортепиано',
     addExpenses = 
@@ -42,10 +15,41 @@ let money = prompt('Ваш месячный доход?', 60000),
     period = 6,
     accumulatedMonth = getAccumulatedMonth(money, getExpensesMonth([amount1, amount2])),
     budgetDay = Math.floor(accumulatedMonth / 30);
-    
-console.log(typeof(money));
-console.log(typeof(income));
-console.log(typeof(deposit));
+
+function showTypeOf(data) {
+  console.log(data, typeof(data));
+};   
+
+function getStatusIncome() {
+  if (budgetDay >= 1200) {
+    return 'У вас высокий уровень дохода';
+  } else if ((budgetDay < 1200) && (budgetDay > 600)) {
+    return 'У вас средний уровень дохода';
+  } else if ((budgetDay <= 600) && (budgetDay >=0 )) {
+    return 'К сожалению у вас уровень дохода ниже среднего';
+  } else {
+    return 'Что то пошло не так';
+  }
+};
+
+//Функция возвращает сумму всех обязательных расходов за месяц
+function getExpensesMonth(expenses) {
+  return expenses.reduce((accume, current) => accume + current);
+};
+
+//Функция возвращает Накопления за месяц (Доходы минус расходы)
+function getAccumulatedMonth(income, expense) {
+  return income - expense;
+};
+
+//Подсчитывает за какой период будет достигнута цель
+function getTargetMonth(amount, expenses) {
+  return amount / expenses;
+};
+
+showTypeOf(money);
+showTypeOf(income);
+showTypeOf(deposit);
 console.log('Накопления за месяц ' + getExpensesMonth([amount1, amount2]));
 console.log(addExpenses);
 console.log(getTargetMonth('Цель будет достигнута за ' + mission, accumulatedMonth + ' месяцев'));
