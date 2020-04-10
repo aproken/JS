acbb140c2a037202c1ff1c313a295655a89f71d3
aproken <aproken@yandex.ru>
2020-04-10 22:05:33 +0300

    feat: add now day

--- lesson07-hard/js/script.js
+++ lesson07-hard/js/script.js
@@ -1,35 +1,20 @@
 'use strict'
 
 let week = ['Воскресенье','Понедельник','Вторник','Среда','Четверг','Пятница','Суббота'],
-    date = new Date(),
-    p1 = document.createElement('p'),
-    p2 = document.createElement('p'),
-    p3 = document.createElement('p'),
-    p4 = document.createElement('p'),
-    p5 = document.createElement('p'),
-    p6 = document.createElement('p'),
-    p0 = document.createElement('p');
+    date = new Date();
     
 function getWeekDay(date) {
   return week[date.getDay()];
 }
 
-p0.className = 'holiday__day';
-p0.innerHTML = week[0];
-p1.innerHTML = week[1];
-p2.innerHTML = week[2];
-p3.innerHTML = week[3];
-p4.innerHTML = week[4];
-p5.innerHTML = week[5];
-p6.className = 'holiday__day';
-p6.innerHTML = week[6];
-// pBold.className = "now__day";
-// pBold.innerHTML = getWeekDay(date);
-
-document.body.append(p0);
-document.body.append(p1);
-document.body.append(p2);
-document.body.append(p3);
-document.body.append(p4);
-document.body.append(p5);
-document.body.append(p6);
+week.forEach(function(element, index) {
+  let day = document.createElement('p');
+  day.innerHTML = element;
+  document.body.append(day);
+  if ((index === 0) || (index === 6)) {
+    day.className = 'holiday__day';
+  }
+  if (element === getWeekDay(date)) {
+    day.className = 'now__day';
+  }
+});
