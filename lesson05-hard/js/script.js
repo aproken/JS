4b6a5da0aae3c6d4be38e2f0c53de5b0404b33db
aproken <aproken@yandex.ru>
2020-04-14 16:45:27 +0300

    feat: add 2 part of task

--- lesson05-hard/js/script.js
+++ lesson05-hard/js/script.js
@@ -4,11 +4,31 @@ let arr = ['234526', '10954', '4532', '9901132','2009','43214', '675437890'];
 
 function getSameElement(arr) {
 
-  return arr.filter(function(str, index) {
+  return arr.filter(function(str) {
     if ((str.slice('')[0] === '2') || (str.slice('')[0] === '4')) {
       return str;
     }
   })
 }
 
+function outSimpleNum(n, m) {
+  let arr = [];
+
+  for (let i = n; i <= m; i ++) {
+    if (i > 1) {
+      arr.push(i);
+    }
+  }
+
+  return arr.filter(function(item) {
+    for (let i = 2; i < (item - 1); i ++) {
+      if ((item % i) === 0) {
+        return false;
+      }
+    }
+    return true;
+  })
+}
+
+console.log(outSimpleNum(1, 100));
 console.log(getSameElement(arr));
