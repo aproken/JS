bdd5b8876fad53342fcb67df41bc0fd53f7450c7
aproken <aproken@yandex.ru>
2020-05-18 11:15:32 +0300

    feat: add lesson19

---
+++ lesson19/js/script.js
@@ -0,0 +1,60 @@
+'use strict';
+
+window.addEventListener('DOMContentLoaded', () => {
+
+let timeId;
+
+  function addZero(item) {
+    if (Math.floor(item / 10) === 0) {
+      return '0' + item;
+    } else { return item; }
+  }
+
+  //Таймер
+  function countTimer(deadline) {
+    const timerHours = document.getElementById('timer-hours'),
+      timerMinutes = document.getElementById('timer-minutes'),
+      timerSeconds = document.getElementById('timer-seconds');
+
+    function getTimeRemaining() {
+      const dateStop = new Date(deadline).getTime(),
+        dateNow = new Date().getTime(),
+        timeRemaining = (dateStop - dateNow) / 1000,
+        
+        seconds = Math.floor(timeRemaining % 60),
+        minutes = Math.floor((timeRemaining / 60) % 60),
+        hours = Math.floor((timeRemaining / 60) / 60);
+        //day = Math.floor(timeRemaining / 60 / 60 / 24);
+      
+        return {
+          hours,
+          minutes,
+          seconds
+        };
+    }
+
+    const updateTimer = function() {
+      const timer = getTimeRemaining();
+
+        if ((timer.hours <= 0) && 
+            (timer.minutes <= 0) && 
+            (timer.seconds <= 0)) {
+          
+              clearInterval(timeId);
+            timerHours.textContent = '00';
+            timerMinutes.textContent = '00';
+            timerSeconds.textContent = '00';
+        }
+        else {
+          timerHours.textContent = addZero(timer.hours);
+          timerMinutes.textContent = addZero(timer.minutes);
+          timerSeconds.textContent = addZero(timer.seconds);
+        }
+        
+      }
+      updateTimer();
+      timeId = setInterval(updateTimer, 1000);
+    }
+  
+  countTimer('19 may 2020');
+});
