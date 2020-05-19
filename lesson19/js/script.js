e04f850573c81eab6beac9b6d4631ffe3b9a4b04
aproken <aproken@yandex.ru>
2020-05-19 20:50:00 +0300

    feat: add smooth scroll

--- lesson19/js/script.js
+++ lesson19/js/script.js
@@ -1,5 +1,15 @@
 'use strict';
 
+let scrollUp = document.createElement('img');
+
+scrollUp.src = './images/sroll-up.png';
+document.body.prepend(scrollUp);
+
+scrollUp.style.display = 'block';
+scrollUp.style.position = 'fixed';
+scrollUp.style.right = '50px';
+scrollUp.style.bottom = '45px'
+
 window.addEventListener('DOMContentLoaded', () => {
 
 let timeId;
@@ -121,6 +131,33 @@ let timeId;
 
   togglePopUp();
 
+
+  const btnScroll = document.querySelector('a');
+  
+  btnScroll.addEventListener('click', (e) => {
+      e.preventDefault();
+      
+      const blockID = btnScroll.getAttribute('href').substr(1);
+      
+      document.getElementById(blockID).scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+      });
+      console.log(document.getElementById(blockID))
+    })
+
+    //scroll up
+    scrollUp.addEventListener('click', () => {
+      //window.scrollTo(pageXOffset, 0);
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+    }) 
+
+    window.addEventListener('scroll', () => {
+      scrollUp.hidden = (pageYOffset < document.documentElement.clientHeight);
+    });
   
   
 
