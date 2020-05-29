376e53d65a3dc1405dab66232c599ed80d082d59
aproken <aproken@yandex.ru>
2020-05-29 19:49:11 +0300

    feat: add valitation in form

---
+++ validator/js/script.js
@@ -0,0 +1,29 @@
+'use strict'
+
+maskPhone('#phone');
+
+const myForm = document.getElementById('myForm'),
+  elementsForm = [];
+
+for (const elem of myForm.elements) {
+  if (elem.tagName.toLowerCase() !== 'button' &&
+  elem.type !== 'button') {
+    elementsForm.push(elem);
+  }
+}
+
+console.log(elementsForm);
+
+const valid = (event) => {
+  elementsForm.forEach(elem => {
+    if (!elem.value) {
+      elem.style.border = 'solid tomato';
+      event.preventDefault();
+    } else {
+      elem.style.border = '';
+    }
+  })
+} 
+
+myForm.addEventListener('submit', valid);
+
