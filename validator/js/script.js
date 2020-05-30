5c154c239949cca6132be0e649a8540bfe35d09b
aproken <aproken@yandex.ru>
2020-05-30 17:43:14 +0300

    feat: add validator.js

--- validator/js/script.js
+++ validator/js/script.js
@@ -1,29 +1,29 @@
-'use strict'
+'use strict';
 
-maskPhone('#phone');
+//maskPhone('#phone');
 
-const myForm = document.getElementById('myForm'),
-  elementsForm = [];
+// const myForm = document.getElementById('myForm'),
+//   elementsForm = [];
 
-for (const elem of myForm.elements) {
-  if (elem.tagName.toLowerCase() !== 'button' &&
-  elem.type !== 'button') {
-    elementsForm.push(elem);
-  }
-}
+// for (const elem of myForm.elements) {
+//   if (elem.tagName.toLowerCase() !== 'button' &&
+//   elem.type !== 'button') {
+//     elementsForm.push(elem);
+//   }
+// }
 
-console.log(elementsForm);
+// console.log(elementsForm);
 
-const valid = (event) => {
-  elementsForm.forEach(elem => {
-    if (!elem.value) {
-      elem.style.border = 'solid tomato';
-      event.preventDefault();
-    } else {
-      elem.style.border = '';
-    }
-  })
-} 
+// const valid = (event) => {
+//   elementsForm.forEach(elem => {
+//     if (!elem.value) {
+//       elem.style.border = 'solid tomato';
+//       event.preventDefault();
+//     } else {
+//       elem.style.border = '';
+//     }
+//   })
+// } 
 
-myForm.addEventListener('submit', valid);
+// myForm.addEventListener('submit', valid);
 
